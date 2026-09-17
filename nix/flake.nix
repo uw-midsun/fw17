63fbb71b96b6452f734612fd06441242e8209922
Lorenzo Good <foehammer@disroot.org>
2026-09-16 19:39:46 -0700

    Initial Commit

---
+++ nix/flake.nix
@@ -0,0 +1,53 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-26.05";
+  };
+
+  outputs =
+    inputs@{ ... }:
+    let
+      supportedSystems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+      forAllSystems = inputs.nixpkgs.lib.genAttrs supportedSystems;
+      buildNixpkgs =
+        system:
+        import inputs.nixpkgs {
+          inherit system;
+          overlays = [ ];
+        };
+    in
+    {
+      devShells = forAllSystems (
+        system:
+        let
+          pkgs = buildNixpkgs system;
+
+          bazelisk-override = pkgs.bazelisk.overrideAttrs (old: {
+            postInstall = (old.postInstall or "") + ''
+              ln -s $out/bin/bazelisk $out/bin/bazel
+            '';
+          });
+        in
+        {
+          default = pkgs.mkShell {
+            buildInputs = with pkgs; [
+              bazelisk-override
+              libtinfo
+              clang-tools
+
+              treefmt
+
+              # Formatters:
+              bazel-buildtools
+              nixpkgs-fmt
+              prettier
+            ];
+          };
+        }
+      );
+    };
+}
